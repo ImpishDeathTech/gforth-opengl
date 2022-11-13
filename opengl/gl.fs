cc5fa829a76c92f7d9fa07e95c3e71d3a4e099c4
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-11-13 19:10:20 +0000

    Update gl.fs

--- opengl/gl.fs
+++ opengl/gl.fs
@@ -1,34 +1,9 @@
-\
+
 \ gl.fs
-\
-\ An unofficial FORTH Port for the Mesa 3-D grapics library
-\
-\ Original C code liscence:
-\ Mesa 3-D graphics library
-\ 
-\ Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
-\ Copyright (C) 2009  VMware, Inc.  All Rights Reserved.
-\ 
-\ Permission is hereby granted, free of charge, to any person obtaining a
-\ copy of this software and associated documentation files (the "Software"),
-\ to deal in the Software without restriction, including without limitation
-\ the rights to use, copy, modify, merge, publish, distribute, sublicense,
-\ and/or sell copies of the Software, and to permit persons to whom the
-\ Software is furnished to do so, subject to the following conditions:
-\ 
-\ The above copyright notice and this permission notice shall be included
-\ in all copies or substantial portions of the Software.
-\ 
-\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-\ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-\ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-\ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-\ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-\ OTHER DEALINGS IN THE SOFTWARE.
-\
-\
-\ FORTH Port Subliscence:
+
+\ This file is not a part of gforth or an official implementation of the Mesa 3-D graphics lib
+
+
 \ BSD 3-Clause License
 \ 
 \ Copyright (c) 2022, Christopher Stephen Rafuse (Fluffykins/Sanguine Night)
@@ -2590,9 +2565,9 @@ GL-TRUE  CONSTANT GLU-TRUE
 : GLU-TRUE? GLU-TRUE  = ;
 
 ( Version )
-GLU-TRUE CONSTANT GLU-VERSION-1.1
-GLU-TRUE CONSTANT GLU-VERSION-1.2
-GLU-TRUE CONSTANT GLU-VERSION-1.3
+GLU-FALSE CONSTANT GLU-VERSION-1.1
+GLU-FALSE CONSTANT GLU-VERSION-1.2
+GLU-FALSE CONSTANT GLU-VERSION-1.3
 
 ( StringName )
 100800 CONSTANT GLU-VERSION   
